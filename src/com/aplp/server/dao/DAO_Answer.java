e24917772ac39bb63174cfad92326f3a2d4249d8
EID Timothée <timothee.eid@gmail.com>
2013-03-06 20:19:36 +0100

    bite

--- src/com/aplp/server/dao/DAO_Answer.java
+++ src/com/aplp/server/dao/DAO_Answer.java
@@ -1,8 +1,18 @@
 package com.aplp.server.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import com.aplp.server.database.Database;
+import com.aplp.shared.businessObjects.Answer;
+import com.aplp.shared.businessObjects.Message;
+import com.aplp.shared.businessObjects.Topic;
+import com.aplp.shared.businessObjects.User;
 
 public class DAO_Answer extends DAO {
 
@@ -13,8 +23,146 @@ public class DAO_Answer extends DAO {
 
 	@Override
 	protected void createTableIfNotExists() throws SQLException {
-		// TODO Auto-generated method stub
+		Statement stat = this.get_database().getConnection().createStatement();
+
+		String req = "" +
+				"CREATE TABLE IF NOT EXISTS " + Answer.TABLE_NAME + " (" +
+				"id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+				"title VARCHAR(20) NOT NULL, " +
+				"message TEXT NOT NULL, " +
+				"creationDate DATE NOT NULL, " +
+				"ownerID INTEGER NOT NULL, " +
+				"answerID INTEGER, " +
+				"topicID INTEGER" +
+				");";
+
+		stat.executeUpdate(req);
+	}
+
+
+
+
+
+
+	public Answer createAnswer(Answer answer) throws SQLException {
+		if(answer == null) {
+			throw new IllegalArgumentException("The \"answer\" argument must not be null");
+		}
+		if(answer.get_id() != null) {
+			throw new IllegalArgumentException("The \"answer\" object must have a null id");
+		}
 		
+		//Prepare the request
+		String sql = "INSERT INTO table_name (title, message, creationDate, ownerID, answerID, topicID)" +
+				"VALUES (?, ?, ?, ?, ?, ?)";
+		Statement stat = this.get_database().getConnection().createStatement();
+	}
+
+
+
+
+	/**
+	 * Return the answers of a message
+	 * @param message Message object
+	 * @return Answer list (may be empty list)
+	 * @throws SQLException 
+	 */
+	public List<Answer> getAnswers(Message message) throws SQLException {
+		if(message instanceof Topic) {
+			return this.getAnswer_Topic((Topic) message);
+		} else if(message instanceof Answer) {
+			return this.getAnswer_Answer((Answer) message);
+		} else {
+			throw new IllegalArgumentException("The \"message\" argument object type is unknown");
+		}
+	}
+
+
+	/**
+	 * Return all the answers of an answer object
+	 * @param answer Reference object 
+	 * @return Answers list
+	 * @throws SQLException 
+	 */
+	private List<Answer> getAnswer_Answer(Answer answer) throws SQLException {
+		if(answer == null) {
+			throw new IllegalArgumentException("The \"answer\" argument must not be null");
+		}
+		if(answer.get_id() == null) {
+			throw new IllegalArgumentException("The \"answer\" object must have a valid id");
+		}
+
+
+		//Prepare the request
+		String sql = "SELECT id, title, message, creationDate, ownerID, answerID, topicID FROM " + Answer	.TABLE_NAME + " WHERE answerID = ?";
+		PreparedStatement stat = this.get_database().getConnection().prepareStatement(sql);
+		stat.setInt(1,  answer.get_id());
+
+		//Execute the request
+		ResultSet result = stat.executeQuery();
+
+		//Test if there is a result
+		List<Answer> answers = new ArrayList<Answer>();
+		while(result.next()) {
+			//Create the answer
+			Integer id = result.getInt(1);
+			String title = result.getString(2);
+			String message = result.getString(3);
+			Date creationDate = new Date(result.getDate(4).getTime());
+			Integer ownerId = result.getInt(5);
+			Integer answerId = result.getInt(6);
+			Integer topicId = result.getInt(7);
+			Answer newAnswer = new Answer(id, title, message, creationDate, ownerId, topicId, answerId);
+
+			//Add it to the list
+			answers.add(newAnswer);
+		}
+
+		return answers;
+	}
+
+
+	/**
+	 * Return all the answers of an topic object
+	 * @param answer Reference object 
+	 * @return Answers list
+	 * @throws SQLException 
+	 */
+	private List<Answer> getAnswer_Topic(Topic topic) throws SQLException {
+		if(topic == null) {
+			throw new IllegalArgumentException("The \"topic\" argument must not be null");
+		}
+		if(topic.get_id() == null) {
+			throw new IllegalArgumentException("The \"topic\" object must have a valid id");
+		}
+
+
+		//Prepare the request
+		String sql = "SELECT id, title, message, creationDate, ownerID, answerID, topicID FROM " + Answer	.TABLE_NAME + " WHERE topicID = ?";
+		PreparedStatement stat = this.get_database().getConnection().prepareStatement(sql);
+		stat.setInt(1,  topic.get_id());
+
+		//Execute the request
+		ResultSet result = stat.executeQuery();
+
+		//Test if there is a result
+		List<Answer> answers = new ArrayList<Answer>();
+		while(result.next()) {
+			//Create the answer
+			Integer id = result.getInt(1);
+			String title = result.getString(2);
+			String message = result.getString(3);
+			Date creationDate = new Date(result.getDate(4).getTime());
+			Integer ownerId = result.getInt(5);
+			Integer answerId = result.getInt(6);
+			Integer topicId = result.getInt(7);
+			Answer newAnswer = new Answer(id, title, message, creationDate, ownerId, topicId, answerId);
+
+			//Add it to the list
+			answers.add(newAnswer);
+		}
+
+		return answers;
 	}
 
 }
--- src/com/aplp/shared/businessObjects/Answer.java
+++ src/com/aplp/shared/businessObjects/Answer.java
@@ -4,30 +4,51 @@ import java.util.Date;
 
 public class Answer extends Message {
 	private static final long serialVersionUID = 6160444405072755646L;
-	private Integer _parentMessageId;
+	
+	public static final String TABLE_NAME = "Answer";
+
+	private Integer _parentTopicId;
+	private Integer _parentAnswerId;
 	
 	
 	public Answer() {
-		this._parentMessageId = null;
+		this.set_parentTopicId(null);
+		this.set_parentAnswerId(null);
 	}
 
-	public Answer(Integer id, String title, String text, Date creationDate, Integer ownerId, Integer parentMessageId) {
+	public Answer(Integer id, String title, String text, Date creationDate, Integer ownerId, Integer parentTopicId, Integer parentAnswerId) {
 		super(id, title, text, creationDate, ownerId);
-		this.set_parentMessageId(parentMessageId);
+		this.set_parentTopicId(parentTopicId);
+		this.set_parentAnswerId(parentAnswerId);
 	}
-	public Answer(String title, String text, Date creationDate, Integer ownerId, Integer parentMessageId) {
+	public Answer(String title, String text, Date creationDate, Integer ownerId, Integer parentTopicId, Integer parentAnswerId) {
 		super(title, text, creationDate, ownerId);
-		this.set_parentMessageId(parentMessageId);
+		this.set_parentTopicId(parentTopicId);
+		this.set_parentAnswerId(parentAnswerId);
+	}
+
+	
+	
+	
+	public Integer get_parentTopicId() {
+		return _parentTopicId;
+	}
+
+	public void set_parentTopicId(Integer _parentTopicId) {
+		this._parentTopicId = _parentTopicId;
+	}
+
+	public Integer get_parentAnswerId() {
+		return _parentAnswerId;
+	}
+
+	public void set_parentAnswerId(Integer _parentAnswerId) {
+		this._parentAnswerId = _parentAnswerId;
 	}
 	
 	
 	
 	
-	public Integer get_parentMessageId() {
-		return _parentMessageId;
-	}
-	public void set_parentMessageId(Integer _parentMessageId) {
-		this._parentMessageId = _parentMessageId;
-	}
+
 
 }
